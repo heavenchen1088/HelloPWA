3baa0a8bfc6454fa4d6cb3c76e1562ecc9a47dae
heavenchen1088 <60906209+heavenchen1088@users.noreply.github.com>
2020-02-11 09:22:09 +0800

    Add files via upload

---
+++ HelloPWA/sw.js
@@ -0,0 +1,33 @@
+var cacheName = 'Hello-PWA';
+
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/main.js'
+];
+
+/* start the service worker and cache all of the app's */
+
+self.addEventListener('install',function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(
+            function(cache) {
+                return cache.addAll(filesToCache);
+            }
+        )
+    );
+});
+
+
+/* serve cache content when offline */
+
+self.addEventListener('fetch',function(e) {
+    e.respondWith (
+        caches.match(e.request).then(
+            function(response) {
+                return response || fetch(e.request);
+            }
+        )
+    );
+});
